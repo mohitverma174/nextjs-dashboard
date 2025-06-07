fb2bb6fcda6e20c702cf135c82432d429570aa4b
Mohit verma <mohit1kverma@gmail.com>
2025-06-07 23:50:47 +0530

    upto 16

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,9 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
 const FormSchema = z.object({
@@ -36,6 +39,7 @@ export type State = {
 
 
 export async function createInvoice(prevState: State, formData: FormData) {
+  //const { customerId, amount, status } = CreateInvoice.parse({
   const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -71,7 +75,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 export async function updateInvoice(
   id: string,
-  _prevState: State,
+  prevState: State,
   formData: FormData,
 ) {
   const validatedFields = UpdateInvoice.safeParse({
@@ -104,7 +108,25 @@ export async function updateInvoice(
   redirect('/dashboard/invoices');
 }
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
+  //throw new Error('Failed to Delete Invoice');
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+}
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -7,9 +7,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams(); 
   const pathname = usePathname();
   const { replace } = useRouter(); 
+
+
   const handleSearch = useDebouncedCallback((term) => {
-  console.log(`Searching... ${term}`);
- 
   const params = new URLSearchParams(searchParams);
   params.set('page', '1');
   if (term) {
