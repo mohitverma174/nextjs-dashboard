7c51646a9fa9114ee79fdb5334a042e0e5d0f19f
Mohit verma <mohit1kverma@gmail.com>
2025-06-10 21:38:27 +0530

    eslint removed

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,20 +1,18 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data';
 import {Suspense} from 'react';
 import CardWrapper from '@/app/ui/dashboard/cards';
-import { RevenueChartSkeleton,LatestInvoicesSkeleton,CardSkeleton, CardsSkeleton} from '@/app/ui/skeletons';
+import { RevenueChartSkeleton,LatestInvoicesSkeleton,CardsSkeleton} from '@/app/ui/skeletons';
  
 export default async function Page() {
   //const latestInvoices = await fetchLatestInvoices();
-  // const {
-  //   numberOfInvoices,
-  //   numberOfCustomers,
-  //   totalPaidInvoices,
-  //   totalPendingInvoices,
-  // } = await fetchCardData();
+   //const {
+     //numberOfInvoices,
+     //numberOfCustomers,
+     //totalPaidInvoices,
+     //totalPendingInvoices,
+   //} = await fetchCardData();
 
   
   return (
